1fc4b5e43d08197a8bac4e1882d6e78efed6df9e
YadinaM <yadina.moreira@gmail.com>
2022-12-29 11:02:33 +0100

    start

---
+++ App.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import drinks from './screens/drinks.js';
+import info from './screens/info.js';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="all drinks" component={(drinks)} />
+        <Stack.Screen name="info" component={(info)} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
