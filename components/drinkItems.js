485fb10d9211135f51fd6bbb7fd4f7b603fe9e3b
YadinaM <yadina.moreira@gmail.com>
2022-12-29 14:42:00 +0100

    niet veel veranderd, doorklikken naar info scherm gaat

--- components/drinkItems.js
+++ components/drinkItems.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet, Pressable, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native'; //nieuw onderdeel
+
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Pressable, Image, Button, ImageBackground } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const drinkItems = props => {
+    const navigation = useNavigation(); //nieuw onderdeel, iemage doet nog niks
     return (
         <View style={styles.list}>
-            <Text>{props.title}</Text>
-                <Pressable onPress ={()=> navigation.navigate ("info")}>
-                    <Text>bekijk product:</Text>
-                </Pressable>
-           
+            <Image source={(props.image)}></Image>
+
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.discription}>{props.description}</Text>
+            <Pressable onPress={() => navigation.navigate("info")}> 
+                <Text style={styles.kijk}>View {props.title}</Text>
+            </Pressable>
         </View>
     );
 
@@ -23,11 +30,20 @@ const styles = StyleSheet.create({
       marginHorizontal: 5,
       marginTop: 10,
       marginBottom: 5,
-      borderColor: 'black',
-      borderStyle: 'dashed',
-      borderWidth: 0.5,
+      borderRadius: 2,
       backgroundColor: "lightblue",
+    },
+    title:{
+        fontSize: 18,
+    },
+    discription:{
+        fontSize: 12,
+        paddingTop: 10,
+    },
+    kijk:{
+        marginTop: 10,
     }
+
   });
 
   export default drinkItems;
